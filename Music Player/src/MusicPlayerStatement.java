7ce585c761f5368451d273cc6cab553e8efd7056
Abhishek Kumar Mishra <42329734+Abhishekmishra-1@users.noreply.github.com>
2019-03-07 21:33:36 +0530

    Create MusicPlayerStatement.java

---
+++ Music Player/src/MusicPlayerStatement.java
@@ -0,0 +1,79 @@
+Write a Java code to develop a Music player application using do-while loop in Eclipse.
+
+Note: The program has to be in the interactive mode.
+
+
+
+Player must be on if and only if the battery level is greater than 30% and battery percentage will gets reduced by 5% after playing a song completely.
+
+
+
+Input has to be the battery level and the total number of songs. Once you give these two inputs the output has to display "Music On
+
+Playing Song 1
+
+Do you want to play again? : Yes/No"
+
+
+
+If user enters "yes" then output has to be 
+
+"Invalid input string!!Enter Yes/No correctly!!
+
+Do you want to play again? : Yes/No"
+
+
+
+If user enters "Yes" then output has to be
+
+"Playing Song 1
+
+Do you want to play again? : Yes/No"
+
+
+
+If user enters "No" then output has to be 
+
+"Do you want to continue with next song?:Yes/No"
+
+
+
+If user enters "Yes" then output has to be 
+
+"Playing song 2
+
+Do you want to play again? : Yes/No"
+
+
+
+If user enters "No" then output has to be
+
+"Do you want to continue with next song?:Yes/No"
+
+
+
+If user enters "No" then output has to be
+
+"Do you want to Stop Music?:Yes/No" 
+
+
+
+If user enters "Yes" then output has to be 
+
+"Music Player Stopped!!!"
+
+
+
+If user enters "No" then output has to be
+
+"RESTARTING" (If last played song is the last song of the play-list)
+
+"RESUME" (else, Resume playing)
+
+
+
+All these should happen when the battery level is greater than 30. If it is less than 30 then output has to be
+
+"Low Battery!!!!
+
+Music Player Stopped!!!"
